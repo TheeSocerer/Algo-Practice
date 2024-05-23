dc3c597d1dc545b9f68bd5cab5a892510e2cfe90
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-05-23 05:27:59 +0200

    done with the function

--- GenerateParentheses/src/main/java/org/example/Main.java
+++ GenerateParentheses/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Let's backtrack");
+        List<String> result = new ArrayList<>();
+
+        generateParenthesis(3,0,0,"",result);
+
+        System.out.println("The output of the generateParanthesis is : "+ result);
     }
+
+    public static void generateParenthesis(int num, int left, int right, String s, List<String> result) {
+        if (s.length() == num*2) {
+            result.add(s);
+            return;}
+        if (left < num) {
+            generateParenthesis(num, left+1, right, s+"(", result);
+        }
+        if (right < left) {
+            generateParenthesis(num, left, right+1, s+")", result);
+        }
+    }
+
 }
