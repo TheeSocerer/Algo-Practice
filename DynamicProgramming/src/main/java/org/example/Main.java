e05e91f4f3030b1501413464bae0e3bcd543dea2
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-05-25 19:58:53 +0200

    Added the canSum function that return a boolean if the array has a combination reaching the number

--- DynamicProgramming/src/main/java/org/example/Main.java
+++ DynamicProgramming/src/main/java/org/example/Main.java
@@ -1,14 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
+//O(n^m) time
+//O(m) space
 
+//after memoization
+//O(n*m)
+//O(m)
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Main dp = new Main();
         Hashtable<String, Integer> gridTraveller = new Hashtable<>();
 //        System.out.println("The fib of 8 is " +dp.fib(50,new Hashtable<>()));
-        System.out.println("You can travel using this many paths " +dp.gridTraveller(16,16,gridTraveller));
+//        System.out.println("You can travel using this many paths " +dp.gridTraveller(16,16,gridTraveller));
+
+//        Hashtable<Integer, Boolean> canSum = new Hashtable<>();
+//        System.out.println(dp.canSum(9,new int[]{2,4,2,6}, canSum));
+//        System.out.println(dp.canSum(11,new int[]{5,4,2,3},new Hashtable<>()) );
+//        System.out.println(dp.canSum(10102,new int[]{3,4,2,1}, new Hashtable<>()) );
+//        System.out.println(dp.canSum(1,new int[]{2,4,2,3}, new Hashtable<>()) );
+
+
+        System.out.println(dp.howSum(9,new int[]{2,4,2,6}));
+        System.out.println(dp.howSum(11,new int[]{5,4,2,3}) );
+        System.out.println(dp.howSum(10,new int[]{3,4,2,1}) );
+        System.out.println(dp.howSum(1,new int[]{2,4,2,3}) );
+
     }
 
     //memoization
@@ -39,4 +59,28 @@ public class Main {
         memo.put(key2,memo.get(key));
         return memo.get(key);
     }
+
+    public boolean canSum(int targetSum, int[] numbers, Hashtable<Integer, Boolean> memo){
+        if(memo.containsKey(targetSum)){
+            return memo.get(targetSum);
+        }
+        if(targetSum == 0){
+            return true;
+        }
+        if(targetSum<0){
+            return false;
+        }
+
+        for(int i: numbers){
+            int remainder = targetSum - i;
+            if (canSum(remainder,numbers, memo)){
+                memo.put(targetSum,true);
+                return true;
+            }
+        }
+        memo.put(targetSum, true);
+        return false;
+    }
+
+
 }
