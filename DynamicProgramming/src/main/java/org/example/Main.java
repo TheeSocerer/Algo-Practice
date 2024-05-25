16aca3a2af8778916efe53adf9e1473a24a547c3
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-05-25 15:59:55 +0200

    did gridTraveller class recursively

--- DynamicProgramming/src/main/java/org/example/Main.java
+++ DynamicProgramming/src/main/java/org/example/Main.java
@@ -7,7 +7,8 @@ public class Main {
         System.out.println("Hello world!");
         Main dp = new Main();
         Hashtable<Integer, Integer> hashtable = new Hashtable<>();
-        System.out.println("The fib of 8 is " +dp.fib(50,new Hashtable<>()));
+//        System.out.println("The fib of 8 is " +dp.fib(50,new Hashtable<>()));
+        System.out.println("You can travel using this many paths " +dp.gridTraveller(3,3));
     }
 
     //memoization
@@ -20,5 +21,16 @@ public class Main {
         }
         memo.put(n,fib(n-1,memo) + fib(n-2,memo));
         return memo.get(n);
+    }
+
+    public int gridTraveller(int m, int n){
+
+        if(m== 1 || n==1){
+            return 1;
+        }
+        if(m==0 || n==0){
+            return 0;
+        }
+        return gridTraveller(m-1,n) + gridTraveller(m,n-1);
     }
 }
